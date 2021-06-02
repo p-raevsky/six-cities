c5e0749af1647c569b18a85d1860cd57764686aa
Павел <raevsky3@gmail.com>
2021-06-02 18:04:02 +0300

    Adds react components: app.jsx, main-page.jsx, place-card.jsx

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MainPage from '../main-page/main-page';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App(props) {
+  const {places} = props;
+
+  return (
+    <MainPage places = {places} />
+  );
 }
+
+App.propTypes = {
+  places: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default App;
