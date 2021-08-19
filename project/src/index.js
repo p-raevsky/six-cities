c6e357c2a773a5d39e110ea054e69d0e73a1538b
Павел <raevsky3@gmail.com>
2021-08-19 11:07:26 +0300

    Adds HashRouter instead of Router

--- project/src/index.js
+++ project/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router as BrowserRouter} from 'react-router-dom';
+import {HashRouter as BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {configureStore} from '@reduxjs/toolkit';
 
